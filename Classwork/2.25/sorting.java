41005588a20970777e0c334d2160b58f5a57d7c5
Javis Wu <wujavis@gmail.com>
2015-02-25 09:17:27 -0500

    2.25

---
+++ Classwork/2.25/sorting.java
@@ -0,0 +1,14 @@
+//insertion insert nth elemnt in already sublist of length n-1
+//selection select nth smallest value and swap with elment in location n
+//sort deck low card on top
+//split in two halves pass on to two other people keep going 
+//best to break into just one card.
+//everyone sorts
+//then give back to giver, giver now has to merge two stacks
+//look at smallest of each and sort
+//mergesort
+//R1 mergesort(A)
+//R2 mergesort(B)
+//return merge(R1,R2)
+//base case if list length is one, return the list
+//
