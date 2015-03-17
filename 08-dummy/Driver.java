8be2d52164bb62442eddd9997d2e09e8800fe717
Javis Wu <wujavis@gmail.com>
2015-03-17 09:22:55 -0400

    hw 8

---
+++ 08-dummy/Driver.java
@@ -0,0 +1,16 @@
+public class Driver{
+    public static void main(String[] args) {
+	LList l = new LList();
+	l.add("hello");
+	l.add("world");
+	for (int i=0;i<5;i++){
+	    l.add(""+i);
+	}
+	System.out.println(l);
+	System.out.println(l.get(1));
+	System.out.println(l.get(5));
+	System.out.println(l.get(9));
+	l.add(2,"yo");
+	System.out.println(l);
+    }
+}
---
+++ 08-dummy/LList.java
@@ -0,0 +1,42 @@
+public class LList{
+    private int length=0;
+    private Node l=new Node("");
+
+    public void add(String s){
+	Node tmp=new Node(s);
+	tmp.setNext(l.getNext());
+	l.setNext(tmp);
+	length++;
+    }
+		
+    public String toString(){
+	String s = "";
+	Node tmp;;
+	for (tmp=l ; tmp!=null ; tmp=tmp.getNext()){
+	    s = s + tmp + " --> ";
+	}
+	s = s + "null";
+	return s;
+    }
+    public String get(int n){
+	Node tmp=l;
+	if (n>length){
+	    return "list too small";
+	}
+	for(int x=0;x<n+1;x++){
+	    tmp=tmp.getNext(); 
+	}
+	return tmp.getData();
+    }
+    
+    public void add(int n, String s){
+	Node tmp=l;
+	for (int x=0;x<n;x++){
+	    tmp=tmp.getNext();
+	}
+	Node stuff= new Node(s);
+	stuff.setNext(tmp.getNext());
+	tmp.setNext(stuff);
+	length++;
+    }
+}
