75c20e2bfcbbd29e47f2d83ea559a116bb47959e
Javis Wu <wujavis@gmail.com>
2015-04-01 09:22:31 -0400

    started spring

---
+++ 12-breadth/coor.java
@@ -0,0 +1,14 @@
+public class coor{
+    int xcor;
+    int ycor;
+    public coor(int x, int y){
+	xcor=x;
+	ycor=y;
+    }
+    public int getx(){
+	return xcor;
+    }
+    public int gety(){
+	return ycor;
+    }
+}
---
+++ 12-breadth/myQueue.java
@@ -0,0 +1,50 @@
+public class myQueue<E>{
+    Node<E> dummy;
+    Node<E> end;
+    public myQueue(){
+	dummy=new Node<E>(null);
+	end=dummy;
+    }
+    public boolean empty(){
+	return dummy==end;
+    }
+    public void enqueue(E data){
+	Node<E> stuff = new Node<E>(data);
+	end.setNext(stuff);
+	end=end.getNext();
+    }
+    public E dequeue(){
+	E tmp = dummy.getNext().getData();
+	dummy.setNext(dummy.getNext().getNext());
+	return tmp;
+    }
+    public E head(){
+	return dummy.getNext().getData();
+    }
+    public String toString(){
+	String s = "";
+	for (Node tmp = dummy; tmp!=end;tmp=tmp.getNext()){
+	    s+=tmp+"-->";
+	}
+	s+=end;
+	return s;
+    }
+}public class myQueue<E>{
+    Node<E> dummy;
+    Node<E> end;
+    ;
+    public myQueue<E>(){
+	dummy=new Node<E>(null);
+	end=dummy;
+    }
+    public void enqueue(E data){
+	Node<E> stuff = new Node<E>(data);
+	end.setNext(stuff);
+	end=end.getNext();
+    }
+    public E dequeue(){
+	E tmp = dummy.getNext().getData();
+	dummy.setNext(dummy.getNext().getNext());
+	return tmp;
+    }
+}
