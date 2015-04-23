78e9c5aa0b1aeaef2940ddffc296767a26e3f375
Javis Wu <wujavis@gmail.com>
2015-04-23 09:23:45 -0400

    started tree

---
+++ 14-tree/binaryT.java
@@ -0,0 +1,12 @@
+public class binaryT{
+    private node root;
+    public binaryT(node root){
+	this.root = root;
+    }
+    public node search(node t, int i){
+	node t1=t;
+	node t2=t;
+	
+    }
+    
+}
---
+++ 14-tree/node.java
@@ -0,0 +1,22 @@
+public class node{
+    private node right;
+    private node left;
+    private int data;
+    public node(int d){
+	data=d;
+	right=null;
+	left=null;
+    }
+    public void setRight(node r){
+	right=r;
+    }
+    public void setLeft(node l){
+	left=l;
+    }
+    public node getRight(){
+	return right;
+    }
+    public node getLeft(){
+	return left;
+    }
+]
