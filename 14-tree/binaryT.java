e50540d87b4ce77bb3cd50a2f9892ed8d365dfbd
Javis Wu <wujavis@gmail.com>
2015-04-29 09:23:04 -0400

    doing remove

--- 14-tree/binaryT.java
+++ 14-tree/binaryT.java
@@ -65,20 +65,7 @@ public class binaryT{
     public String traverse(node t){
         
 	if (t==null){
-	    //lineup.remove(0);
-	    // System.out.println(lineup);
-	    /*
-	    for (int x=0;x<lineup.size();x++){
-		System.out.println(x);
-		if(lineup.get(x)==null){
-		    lineup.remove(lineup.get(x));
-		}
-		traverse(lineup.get(x));
-		/*if (!(x==null)){
-		    traverse(x);
-		}
-		else
-		return " ";*/
+	   
 	    return " ";
 			
 	    }
@@ -86,41 +73,43 @@ public class binaryT{
 	    
 	
 	else{
-	    /* lineup.add(lineup.get(0).getLeft());
-	    lineup.add(lineup.get(0).getRight());
-	    //System.out.println(lineup);
-	    limit++;
-	    if (limit==Math.pow(2.0,power)){
-		power++;
-		return "\n  "+lineup.remove(0)+traverse(lineup.get(0))+"";
-		
-	    }
-	    else
-	    return "  "+lineup.remove(0)+traverse(lineup.get(0))+"  ";
-	    //return t+"\n"+traverse(t.getLeft())+traverse(t.getRight())+"\n";
-	    }*/
+	    
 	   return traverse(t.getLeft())+t.getData()+", "+traverse(t.getRight());
 		
 	
 	
 	}
     }
+    public remove(int i){
+	node t = root;
+	node before=null;
+	while (t!=null){
+	    Integer holder=new Integer(t.getData());
+	    Integer holder2=new Integer(i);
+	    int c = holder.compareTo(holder2);
+	    if (c>0){
+		before=t;
+		t=t.getLeft();
+	        
+	    }
+	    else {
+		before=t;
+		t=t.getRight();
+	        
+	    }
+	}
+	if (t.getRight()==null && before.getData()>i){
+	    before.setLeft(t.getLeft());
+	}
+	else if(t.getRight()==null){
+	    before.setRight(t.getLeft());
+	}
+    }
     public String toString(){
 	
-	lineup.add(root);
-	return traverse(root);
-	/*	System.out.println(root.getData());
-	System.out.println(root.getLeft().getData());
-	System.out.println(root.getRight().getData());
-	System.out.println();
-	System.out.println("  "+root.getLeft().getData());
-	System.out.println(root.getLeft().getLeft().getData()+" "+root.getLeft().getRight().getData());
 
-	System.out.println();
-	System.out.println("  "+root.getRight().getData());
-	System.out.println(root.getRight().getLeft().getData()+" "+root.getRight().getRight().getData());
-	return "done";
-	*/		   
+	return traverse(root);
+	
 	}
 	
 }
